ac2273d740af6fa3fc3c63fc0c680e05ba20655b
sakchaisrim <sakchaisrim@gmail.com>
2022-11-17 14:16:20 +0700

    1st

---
+++ lib/1_var.dart
@@ -0,0 +1,25 @@
+// 1_var.dart
+void main(){
+  // สร้างตัวแปร แบบ ไม่กำหนดค่า
+  // dart จะเห็นเป็นค่า null
+  int? x;
+  double? d;
+  bool? isDone;
+  String? name;
+
+  print(x);
+  print(d);
+  print(isDone);
+  print(name);
+
+  int yy=100;
+  double dd = 50.0;
+  bool isOk = true;
+  String fname = "sakchai";
+
+  print("$yy\n$dd\n$isOk\n$fname");
+
+
+
+
+}
---
+++ lib/dart_demo.dart
@@ -0,0 +1,14 @@
+int calculate() {
+  return 6 * 7;
+}
+
+void main() {
+  print(calculate());
+  print("My laarn " + "dart programming");
+  print("My name is sakchai \nI'm developer");
+  print("""How to learn flutter
+way this moment 
+and go to production in tomorrow""");
+  print(r"This is \nis\n multiline");
+
+}
